88b362051844268d52e474ad55a7253ee4211e11
Swapnil Khande <swapnilkhande6996@gmail.com>
2023-09-13 11:49:26 +0530

    Done with user login & registration

--- server.js
+++ server.js
@@ -1,7 +1,65 @@
-const express = require('express');
-  
+const express = require("express");
+const { main } = require("./models/index");
+const cors = require("cors");
+const User = require("./models/users");
+const alert = require('alert');
+
 const app = express();
 const PORT = 4000;
+main();
+app.use(express.json());
+app.use(cors());
+// ------------- Signin --------------
+let userAuthCheck;
+app.post("/api/login", async (req, res) => {
+  console.log(req.body);
+  // res.send("hi");
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    console.log("USER: ", user);
+    if (user) {
+      res.send(user);
+      userAuthCheck = user;
+    } else {
+      res.status(401).send("Invalid Credentials");
+      userAuthCheck = null;
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+// Getting User Details of login user
+app.get("/api/login", (req, res) => {
+  res.send(userAuthCheck);
+});
+// ------------------------------------
+
+// Registration API
+app.post("/api/register", (req, res) => {
+  let registerUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    phoneNumber: req.body.phoneNumber,
+    imageUrl: req.body.imageUrl,
+  });
+
+  registerUser
+    .save()
+    .then((result) => {
+      res.status(200).send(result);
+      alert("Signup Successfull");
+    })
+    .catch((err) => console.log("Signup: ", err));
+  console.log("request: ", req.body);
+});
+
   
 app.listen(PORT, (error) =>{
     if(!error)
